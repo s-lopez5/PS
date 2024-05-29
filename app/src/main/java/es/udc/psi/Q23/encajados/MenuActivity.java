8890abedf9526680634d9984c9eb645ed371dde7
Santi Lopez <s.lopez5@udc.es>
2024-05-30 00:32:06 +0200

    Añadido settings

--- app/src/main/java/es/udc/psi/Q23/encajados/MenuActivity.java
+++ app/src/main/java/es/udc/psi/Q23/encajados/MenuActivity.java
@@ -1,13 +1,21 @@
 package es.udc.psi.Q23.encajados;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
     String TAG = "_TAG";
@@ -48,4 +56,25 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_settings) {
+            Log.d(TAG, "menu settings");
+
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
